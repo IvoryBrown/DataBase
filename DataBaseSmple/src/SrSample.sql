354dfacba58aa8554ecd3dc4a8871b81182a47ee
SzervizIvo <paradisho@gmail.com>
2018-02-27 18:35:41 +0100

    Init

--- DataBaseSmple/src/SrSample.sql
+++ DataBaseSmple/src/SrSample.sql
@@ -31,7 +31,6 @@ CREATE TABLE IF NOT EXISTS `szerviz_up`.`ugyfel_adatok` (
   `ugyfel_email` VARCHAR(100) NULL,
   `ugyfel_telefon` VARCHAR(100) NULL,
   `csomag_tipus` VARCHAR(100) NULL,
-  `ugyintezo` VARCHAR(100) NULL,
   `ugyfel_megjegyzes` VARCHAR(255) NULL,
   PRIMARY KEY (`id_ugyfel`));
 
@@ -43,66 +42,51 @@ CREATE TABLE IF NOT EXISTS `szerviz_up`.`gepadatok` (
   `id_gepadatok` INT NOT NULL AUTO_INCREMENT,
   `ugyfel_adatok_id_ugyfel` INT NOT NULL,
   `eszkoz_azonosito` VARCHAR(155) NULL,
-  `eszkoz_fajtaja` VARCHAR(150) NULL,
+  `ceg_nev_gep` VARCHAR(45) NULL,
+  `ugyfél_nev_gep` VARCHAR(45) NULL,
+  `eszkoz` VARCHAR(150) NULL,
   `eszkoz_gyarto` VARCHAR(150) NULL,
-  `eszkoz_tipusa` VARCHAR(150) NULL,
   `eszkoz_gyari_szama` VARCHAR(155) NULL,
-  `tartozekok` VARCHAR(255) NULL,
-  `vasarlasi_datuma` DATE NULL,
-  `bejelentes_datuma` DATETIME NULL,
-  `hatarido_datuma` DATE NULL,
-  `elkeszult_datuma` DATETIME NULL,
-  `uzembehelyezes_datuma` DATE NULL,
-  `kiszallas_datuma` DATETIME NULL,
-  `hiba_leirasa` VARCHAR(255) NULL,
-  `hibajavitas_leirasa` VARCHAR(255) NULL,
   `javitas_helye` VARCHAR(155) NULL,
+  `allapot` VARCHAR(155) NULL,
+  `uj_gep` VARCHAR(45) NULL,
+  `ugyintezo` VARCHAR(45) NULL,
+  `prioritas` VARCHAR(45) NULL,
+  `jelszo` VARCHAR(45) NULL,
+  `hivatkozasi_szam` VARCHAR(85) NULL,
+  `tartozekok` VARCHAR(255) NULL,
+  `serules` VARCHAR(255) NULL,
+  `hiba_leirasa` VARCHAR(255) NULL,
+  `eszkoz_megjegyzes` VARCHAR(255) NULL,
+  `vasarlasi_datuma` DATE NULL,
+  `bejelentes_datuma` DATE NULL,
+  `hatarido_datuma` DATE NULL,
+  `kiszallas_datuma` DATE NULL,
+  `adatmentes` VARCHAR(45) NULL,
+  `softver` VARCHAR(45) NULL,
+  `operacios_rendszer` VARCHAR(45) NULL,
+  `softver_megjegyzés` VARCHAR(255) NULL,
+  `laptop` VARCHAR(10) NULL,
+  `haz` VARCHAR(10) NULL,
+  `tapegyseg` VARCHAR(10) NULL,
+  `processzor` VARCHAR(10) NULL,
+  `alaplap` VARCHAR(10) NULL,
+  `memoria` VARCHAR(10) NULL,
+  `videokartya` VARCHAR(10) NULL,
+  `ssd` VARCHAR(10) NULL,
+  `meghajto` VARCHAR(10) NULL,
+  `hutoventilator` VARCHAR(10) NULL,
+  `optikai_meghajto` VARCHAR(10) NULL,
+  `bovitokartya` VARCHAR(10) NULL,
+  `elkeszult_datuma` DATE NULL,
+  `hibajavitas_leirasa` VARCHAR(255) NULL,
   `anyaghiany_adatok` VARCHAR(255) NULL,
   `technikus` VARCHAR(155) NULL,
-  `statusz` VARCHAR(155) NULL,
-  `eszkoz_megjegyzes` VARCHAR(255) NULL,
   PRIMARY KEY (`id_gepadatok`),
   INDEX `fk_gepadatok_ugyfel_adatok_idx` (`ugyfel_adatok_id_ugyfel` ASC),
   CONSTRAINT `fk_gepadatok_ugyfel_adatok`
     FOREIGN KEY (`ugyfel_adatok_id_ugyfel`)
     REFERENCES `szerviz_up`.`ugyfel_adatok` (`id_ugyfel`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
-
--- -----------------------------------------------------
--- Table `szerviz_up`.`hardver`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `szerviz_up`.`hardver` (
-  `id_hardver` INT NOT NULL AUTO_INCREMENT,
-  `gepadatok_id_hardver` INT NOT NULL,
-  `hardver_fajtaja` VARCHAR(155) NULL,
-  `hardver_gyarto` VARCHAR(155) NULL,
-  `hardver_tipusa` VARCHAR(155) NULL,
-  `hardver_megjegyzes` VARCHAR(255) NULL,
-  PRIMARY KEY (`id_hardver`),
-  INDEX `fk_hardver_gepadatok1_idx` (`gepadatok_id_hardver` ASC),
-  CONSTRAINT `fk_hardver_gepadatok1`
-    FOREIGN KEY (`gepadatok_id_hardver`)
-    REFERENCES `szerviz_up`.`gepadatok` (`id_gepadatok`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
-
--- -----------------------------------------------------
--- Table `szerviz_up`.`softver`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `szerviz_up`.`softver` (
-  `id_softver` INT NOT NULL AUTO_INCREMENT,
-  `gepadatok_id_softver` INT NOT NULL,
-  `softver` VARCHAR(255) NULL,
-  `softver_kulcs` VARCHAR(255) NULL,
-  `softver_megjegyzes` VARCHAR(255) NULL,
-  PRIMARY KEY (`id_softver`),
-  INDEX `fk_softver_gepadatok1_idx` (`gepadatok_id_softver` ASC),
-  CONSTRAINT `fk_softver_gepadatok1`
-    FOREIGN KEY (`gepadatok_id_softver`)
-    REFERENCES `szerviz_up`.`gepadatok` (`id_gepadatok`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
@@ -116,30 +100,19 @@ CREATE TABLE IF NOT EXISTS `szerviz_up`.`raktar` (
   `kelte` DATE NULL,
   `eladas_kelte` DATE NULL,
   `mennyiseg` INT NULL,
+  `raktaron` INT NULL,
   `leiras` VARCHAR(255) NULL,
+  `szamla_azonosito` VARCHAR(255) NULL,
   PRIMARY KEY (`category_id`));
 
 
 -- -----------------------------------------------------
--- Table `szerviz_up`.`ugyfel_adatok_copy1`
+-- Table `szerviz_up`.`ugyintezo`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `szerviz_up`.`ugyfel_adatok_copy1` (
-  `id_ugyfel` INT NOT NULL AUTO_INCREMENT,
-  `ugyfel_azonosito` VARCHAR(155) NULL,
-  `cegnev` VARCHAR(255) NULL,
-  `ugyfel_nev` VARCHAR(100) NULL,
-  `megye` VARCHAR(100) NULL,
-  `telepules` VARCHAR(100) NULL,
-  `iranyitoszam` INT NULL,
-  `cim` VARCHAR(255) NULL,
-  `ceg_telefon` VARCHAR(100) NULL,
-  `ceg_email` VARCHAR(100) NULL,
-  `ugyfel_email` VARCHAR(100) NULL,
-  `ugyfel_telefon` VARCHAR(100) NULL,
-  `csomag_tipus` VARCHAR(100) NULL,
-  `ugyintezo` VARCHAR(100) NULL,
-  `ugyfel_megjegyzes` VARCHAR(255) NULL,
-  PRIMARY KEY (`id_ugyfel`));
+CREATE TABLE IF NOT EXISTS `szerviz_up`.`ugyintezo` (
+  `idugyintezo` INT NOT NULL AUTO_INCREMENT,
+  `ugyintezo_neve` VARCHAR(60) NULL,
+  PRIMARY KEY (`idugyintezo`));
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
